db53e6843133a326461991dc2dfdaa09438165b6
Ayesha Afzal <154336255+AyeshaAfzal-01@users.noreply.github.com>
2025-08-02 21:10:40 +0500

    about2

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -23,6 +23,23 @@ const About = () => {
       <div className='text-xl py-4'>
         <Title text1={'WHY'} text2={'CHOOSE US'}/>
       </div>
+
+      <div className='flex flex-col md:flex-row text-sm mb-20'>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Quality Assaurance:</b>
+          <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime perspiciatis dolorem praesentium.</p>
+        </div>
+
+         <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Convinience:</b>
+          <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime perspiciatis dolorem praesentium.</p>
+        </div>
+
+         <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Exceptional Customer Service:</b>
+          <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime perspiciatis dolorem praesentium.</p>
+        </div>
+      </div>
     </div>
   )
 }
