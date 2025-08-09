f56f438ef8fd85535cbf57a9941d93b189721805
Ayesha Afzal <154336255+AyeshaAfzal-01@users.noreply.github.com>
2025-08-09 19:59:48 +0500

    userLogin controller function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -10,7 +10,28 @@ const createToken = (id) => {
 
 // Route for user login
 const loginUser = async (req, res) => {
+    try {
+        const { email, password} = req.body;
 
+        const user = await userModel.findOne({email})
+        if (!user) { // user not available in DB
+            return res.json({success:false, message:"User does'nt exist"})
+        }
+
+        // if user available then match the password
+        const isMatch = await bcrypt.compare(password, user.password) // user.password is password saved in DB
+        
+        if (isMatch) {
+            const token = createToken(user._id)
+            res.json({success:true, token})
+        }
+        else {
+            res.json({success:false, message: "Invalid credentials"})
+        }
+    } catch (error) {
+        console.log(error)
+        res.json({success:false, message:error.message})
+    }
 }
 
 // Route for user register
