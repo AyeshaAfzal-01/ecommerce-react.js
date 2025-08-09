ffccd1385c1c442d6818bdee9137522054c98fc9
Ayesha Afzal <154336255+AyeshaAfzal-01@users.noreply.github.com>
2025-08-09 21:33:39 +0500

    productController

---
+++ backend/controllers/productController.js
@@ -0,0 +1,20 @@
+// controller function to add the products
+// function for add product
+const addProduct = async () => {
+
+}
+
+// function for list product
+const listProduct = async () => {
+
+}
+
+const removeProduct = async () => {
+
+}
+
+const singleProduct = async () => {
+
+}
+
+export default { addProduct, listProduct, removeProduct, singleProduct }
