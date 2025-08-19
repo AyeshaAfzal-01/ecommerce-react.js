93531cfa20b83b7362db3bd563c5d9c8f0f774f3
Ayesha Afzal <ayesha.taisee1@gmail.com>
2025-08-19 16:26:50 +0500

    product controller api's

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -10,7 +10,7 @@ const addProduct = async (req, res) => {
         if (isNaN(priceNum) || priceNum <= 0) {
             return res.json({ success: false, message: "price should be a positive number" })
         }
-        
+
         // uploading images to cloudinary
         const uploadToCloudinary = (file) => { // function to upload a file to cloudinary
             return cloudinary.uploader.upload(file.path, {
@@ -18,7 +18,7 @@ const addProduct = async (req, res) => {
                 resource_type: "image"
             })
         }
-        
+
         // getting image files from multer
         const files = [
             req.files?.image1?.[0],
@@ -31,8 +31,8 @@ const addProduct = async (req, res) => {
         const results = await Promise.all(files.map(file => uploadToCloudinary(file)))
         // images is an array of imageUrLs
         const images = results.map(r => r.secure_url)
-        if (images.length === 0 ) {
-            return res.json({success: false, message: "upload atleast one image of product"})
+        if (images.length === 0) {
+            return res.json({ success: false, message: "upload atleast one image of product" })
         }
 
         const newProduct = new productModel({
@@ -56,17 +56,39 @@ const addProduct = async (req, res) => {
 
 // function to remove product
 const removeProduct = async (req, res) => {
-
+    try {
+        await productModel.findByIdAndDelete(req.body.id)
+        res.json({ success: true, message: "product removed" })
+    } catch (error) {
+        console.log(error)
+        return res.json({ success: false, message: error.message })
+    }
 }
 
-// to listProducts
+// list product will get all the products from database
 const listProduct = async (req, res) => {
-
+    try {
+        const products = await productModel.find() // fetch products from DB
+        res.json({ success: true, message: "Product data fetched successfully", products })
+    } catch (error) {
+        console.log(error)
+        return res.json({ success: false, message: error.message })
+    }
 }
 
-// for single product
-const singleProduct = async (req, res) => {
-
+// fetch details of one product by id
+const singleProduct = async (req, res) => { 
+    try {
+        const { productId } = req.body
+        const product = await productModel.findById(productId)
+        if (!product) {
+            return res.json({success: false, message: "product not found"})
+        }
+        res.json({success:true, message:"product found", product})
+    } catch (error) {
+        console.log(error)
+        return res.json({success: false, message: error.message})
+    }
 }
 
 export { addProduct, listProduct, removeProduct, singleProduct }
